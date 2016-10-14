e38d7620656a68a06cc718488baa90fbfa3981cc
Oscar <oscar.mendez.soft@gmail.com>
2016-10-14 11:02:02 -0500

    Se agrego la funcion multiplicacion

--- suma.php
+++ suma.php
@@ -3,6 +3,8 @@ $numer1 = 5;
 $numero2 = 5;
 $suma = $numero1+$numero2;
 $resta = $numero1-$numero2;
+$multiplicacion = $numero1*$numero2;
 echo "Suma: ".$suma."<p>";
 echo "Resta: ".$resta."<p>";
+echo "Multiplicacion: ".$multiplicacion."<p>";
 ?>
